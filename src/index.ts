a04131e8b565155048fe10ed5e5d56c748e54e74
Youngjin Mo <dev.youngjinmo@gmail.com>
2023-03-15 10:54:23 +0900

    Refactor to export FastCache interfaces from root

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-import { FastCache } from './FastCache';
+import { FastCache, FastCacheOpts, ListOperations, MapOperations, SetOperations } from './FastCache';
 import { InMemoryCache } from './InMemoryCache';
 
-export { InMemoryCache, FastCache, FastCache as RedisCache };
+export {
+  FastCache,
+  FastCache as RedisCache,
+  FastCacheOpts,
+  InMemoryCache,
+  ListOperations,
+  MapOperations,
+  SetOperations,
+};
