25913ed7550e87af3158dfe9707aa52135546fd4
Youngjin Mo <dev.youngjinmo@gmail.com>
2023-03-15 13:07:25 +0900

    Refactor export statement of root

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
-import { FastCache, FastCacheOpts, ListOperations, MapOperations, SetOperations } from './FastCache';
-import { InMemoryCache } from './InMemoryCache';
-
 export {
   FastCache,
   FastCache as RedisCache,
   FastCacheOpts,
   FastCacheOpts as RedisCacheOpts,
-  InMemoryCache,
   ListOperations,
   MapOperations,
   SetOperations,
-};
+} from './FastCache';
+export { InMemoryCache } from './InMemoryCache';
